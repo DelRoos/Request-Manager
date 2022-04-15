8dff5bcddd230d4cc63ed9bd9d0dd9134ac1155e
DelRoos <delanoroosvelt733@gmail.com>
2022-04-15 21:15:26 +0100

    add config

--- request_manager/settings.py
+++ request_manager/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-1zv9a+buoz+#$k5(h72rd%2py$etbv=!z7n15%z67jh^&yr=b_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
