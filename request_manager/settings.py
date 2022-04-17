0e090ce24a4a2c73a171799805f89ed84f5d7399
joan30122001 <ngounouloic675@gmail.com>
2022-04-17 11:23:53 +0100

    add of account application

--- request_manager/settings.py
+++ request_manager/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'admin_static/')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+AUTH_USER_MODEL = 'account.User'
